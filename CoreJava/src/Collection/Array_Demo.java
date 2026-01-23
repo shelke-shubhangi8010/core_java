01590488bca51af95f8b9bfcb8f7c45b197cb363
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2026-01-23 17:31:20 +0530

    zip file created

---
+++ CoreJava/src/Collection/Array_Demo.java
@@ -0,0 +1,10 @@
+package Collection;
+
+public class Array_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- CoreJava/src/File_Handling/Zip_File_Create.java
+++ CoreJava/src/File_Handling/Zip_File_Create.java
@@ -1,13 +1,46 @@
 package File_Handling;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.util.zip.ZipOutputStream;
+import java.util.zip.ZipEntry;
 public class Zip_File_Create {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  
+	{
 		// TODO Auto-generated method stub
 
-		String sourceFile= "C:\\file data\\sample1.txt";
+		String sourceFile= "C:\\file data\\sample.txt";  //   txt file convert into zip file
 		
+		String  zipFile = "C:\\file data\\output.zip ";     // new zip file created 
 		
+		try {
+		     
+		     FileInputStream fos=new FileInputStream(zipFile);  // send data
+		     ZipOutputStream zos =new ZipOutputStream(fos);
+		     
+		     File f1 = new File(sourceFile);
+		     
+		    FileInputStream fis=new FileInputStream(f1);   // send data
+		    ZipEntry entry = new ZipEntry(f1.getName());
+		    zos.putNextEntry(entry);
+		    
+		    byte[] bytes = new byte[1024];
+		    int length;
+		    while((length = fos.read())>=0);
+		    {
+		    	  zos.write(bytes,0,length);
+		    	  
+		    }
+		    zos.close();
+		    fos.close();
+		    fis.close();
+		    System.out.println("file is created");
+		    
+		}catch(Exception e) 
+		{
+			System.out.println(e);
+		}
 	}
 
 }
